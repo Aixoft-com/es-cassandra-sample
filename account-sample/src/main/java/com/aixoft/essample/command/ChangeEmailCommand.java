3817b04be1583e884a384d0b1a899a9c8b75d08c
Piotr Stój <piotr.stoj@aixoft.com>
2022-06-27 21:41:54 +0200

    update samples

--- account-sample/src/main/java/com/aixoft/essample/command/ChangeEmailCommand.java
+++ account-sample/src/main/java/com/aixoft/essample/command/ChangeEmailCommand.java
@@ -1,37 +1,27 @@
 package com.aixoft.essample.command;
 
 import com.aixoft.escassandra.aggregate.Aggregate;
-import com.aixoft.escassandra.model.Command;
 import com.aixoft.escassandra.model.Event;
 import com.aixoft.escassandra.model.EventVersion;
+import com.aixoft.escassandra.model.command.VersionedCommand;
 import com.aixoft.essample.aggregate.AccountInformation;
 import com.aixoft.essample.event.EmailModified;
-import com.aixoft.essample.exception.UnexpectedEventVersionException;
 import lombok.Value;
 
 import java.util.List;
 
 @Value
-public class ChangeEmailCommand implements Command<AccountInformation> {
+/**
+ * Example: processing chain is stopped by exception thrown on validation failure
+ * (version of aggregate is not as expected).
+ */
+public class ChangeEmailCommand extends VersionedCommand<AccountInformation> {
     String email;
     EventVersion expectedVersion;
 
-    /**
-     * Example: processing chain is stopped by exception thrown on validation failure
-     * (version of aggregate is not as expected).
-     */
     @Override
-    public boolean validate(Aggregate<AccountInformation> aggregate) {
-        if(aggregate.getCommittedVersion() == null) {
-            throw new UnexpectedEventVersionException("Aggregate not found for given snapshot version");
-        }
-
-        if(expectedVersion != null && !expectedVersion.equals(aggregate.getCommittedVersion())) {
-            throw new UnexpectedEventVersionException(String.format("Current event version is %s.%s",
-                    aggregate.getCommittedVersion().getMajor(),
-                    aggregate.getCommittedVersion().getMinor()));
-        }
-
+    public boolean postValidate(Aggregate<AccountInformation> aggregate) {
+        // Email format can be validated here.
         return true;
     }
 
